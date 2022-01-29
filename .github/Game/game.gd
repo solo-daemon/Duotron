85a0645c0c0544fc1c6852993e5332bdaafb94cd
Ishan Singh <i_singh@ch.iitr.ac.in>
2022-01-29 11:29:07 +0530

    Update game.gd

--- .github/Game/game.gd
+++ .github/Game/game.gd
@@ -3,15 +3,20 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		
-		$"left/-veSign".visible=false
 		$"left/+veSignleft".visible=true
-		
+		$"right/+veSignright".visibe=false
+		$"left/-veSign".visible=false
+		$"right/-veSign".visible=true
 	elif Input.is_action_pressed("ui_right"):
+		$"left/+veSignleft".visible=false
+		$"right/+veSignright".visibe=true
+		$"left/-veSign".visible=true
 		$"right/-veSign".visible=false
-		$"right/+veSignright".visible=true
 	else:
 		$"left/+veSignleft".visible=false
 		$"right/+veSignright".visibe=false
+		$"left/-veSign".visible=true
+		$"right/-veSign".visible=true
 		
 		
 	
