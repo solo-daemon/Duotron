a28eb8ba161795714c38d2ff4a24c717b1207020
jindalj <97535845+jindalj@users.noreply.github.com>
2022-01-29 12:10:54 +0530

    changed

--- .github/Game/game.gd
+++ .github/Game/game.gd
@@ -4,17 +4,17 @@ func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		
 		$"+veSignleft".visible=true
-		$"+veSignright".visibe=false
+		$"+veSignright".visible=false
 		$"-veSignleft".visible=false
 		$"-veSignright".visible=true
 	elif Input.is_action_pressed("ui_right"):
 		$"+veSignleft".visible=false
-		$"+veSignright".visibe=true
+		$"+veSignright".visible=true
 		$"-veSignright".visible=false
 		$"-veSignleft".visible=true
 	else:
 		$"+veSignleft".visible=false
-		$"+veSignright".visibe=false
+		$"+veSignright".visible=false
 		$"-veSignleft".visible=true
 		$"-veSignright".visible=true
 		
